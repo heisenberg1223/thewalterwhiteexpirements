2f2ea540f7726dbfe06124e1e31ece03875e0a65
heisenberg123 <heisenberg123@gmail.com>
2023-12-27 00:04:07 +0100

    removed useless template in the fError and fWarning functions.

--- src/Error/ErrorCallback.hpp
+++ src/Error/ErrorCallback.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 enum ErrorLevels {
-  ERROR_FATAL, ERROR_NORMAL
+  ERROR_FATAL, ERROR_NORMAL, ERROR_NETWORK_FATAL, ERROR_NETWORK_NORMAL
 };
 
 #define ANSI_RED "\x1b[1;31m"
@@ -15,19 +15,28 @@ enum ErrorLevels {
 
 namespace Error {
 
-  template<typename Tx>
-  inline static void fError(Tx desc, ErrorLevels errorlvl) noexcept {
-    if (errorlvl == ERROR_FATAL) {
-      std::cerr << ANSI_RED << "FATAL ERROR: -- " << END_ANSI;
-      std::cerr << desc << '\n';
-    } else if (errorlvl == ERROR_NORMAL) {
-      std::cerr << ANSI_RED << "ERROR: " << END_ANSI;
-      std::cerr << desc << '\n';
+  inline static void fError(const char *desc, ErrorLevels errorlvl) noexcept {
+    switch (errorlvl) {
+      case (ERROR_FATAL):
+        std::cerr << ANSI_RED << "FATAL ERROR: -- " << END_ANSI;
+        std::cerr << desc << '\n';
+        break;
+      case (ERROR_NORMAL):
+        std::cerr << ANSI_RED << "ERROR: " << END_ANSI;
+        std::cerr << desc << '\n';
+        break;
+      case (ERROR_NETWORK_FATAL):
+        std::cerr << ANSI_RED << "FATAL NETWORK ERROR: " << END_ANSI;
+        std::cerr << desc << '\n';
+        break;
+      case (ERROR_NETWORK_NORMAL):
+        std::cerr << ANSI_RED << "NETWORK ERROR: " << END_ANSI;
+        std::cerr << desc << '\n';
+        break;
     }
   }
 
-  template<typename Tx>
-  inline static void fWarning(Tx desc) noexcept {
+  inline static void fWarning(const char *desc) noexcept {
     std::cout << ANSI_YELLOW << "WARNING: -- " << END_ANSI;
     std::cout << desc << '\n';
   }
