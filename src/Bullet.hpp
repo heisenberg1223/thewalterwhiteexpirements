9888a9c9f2c15d2af3c7bcb6abc5d5515ed2b3b0
heisenberg123 <heisenberg123@gmail.com>
2023-12-27 21:08:45 +0100

    minor code optimizations

--- src/Bullet.hpp
+++ src/Bullet.hpp
@@ -22,18 +22,18 @@ class Bullet {
       }
     }
 
-    inline void append(std::pair<short int, short int> pair) {
+    void append(std::pair<short int, short int> pair) {
       bullets.push_back(pair);
     }
 
-    inline void del(std::pair<short int, short int> pair) {
+    void del(std::pair<short int, short int> pair) {
       auto it = std::find(bullets.begin(), bullets.end(), pair);
       if (it != bullets.end()) {
         bullets.erase(it);
       }
     }
 
-    inline void wipe() {
+    void wipe() {
       bullets.clear();
     }
   private:
